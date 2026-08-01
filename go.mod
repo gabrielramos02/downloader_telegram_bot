21f302c0b98f2ea8436eb2dcd04f4e523456532e
gabrielramos02 <gabrielrp02ok@outlook.com>
2026-08-01 09:04:54 +0200

    migrated to external package

--- go.mod
+++ go.mod
@@ -6,5 +6,7 @@ require (
 	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible // indirect
 	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/superturkey650/go-qbittorrent v0.0.0-20250509144237-d119a59ccf27 // indirect
 	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+	golang.org/x/net v0.38.0 // indirect
 )
--- main.go
+++ main.go
@@ -7,13 +7,24 @@ import (
 	"os"
 	"strings"
 
-	qbittorrentapi "github.com/gabrielramos02/telegram-bot-go/internal/qbittorrent_api"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/joho/godotenv"
+	"github.com/superturkey650/go-qbittorrent/qbt"
 )
 
 var bot *tgbotapi.BotAPI
-var qbConnection *qbittorrentapi.QbittorrentAPIClient
+var qb *qbt.Client
+
+const (
+	torrentList = `<pre>
+FILE                      SIZE     STATUS        PROGRESS       SPEED      ETA
+-------------------------------------------------------------------------------
+ubuntu-24.04-desktop.iso  5.8 GB   Downloading   [██████░░░░]   12.5 MB/s  02m 15s
+dataset_v2_final.zip      1.2 GB   Completed     [██████████]   0 MB/s     Done
+video_render_4k.mp4       850 MB   Paused        [███░░░░░░░]   0 MB/s     Paused
+backup_database.sql.gz    320 MB   Failed        [█░░░░░░░░░]   0 MB/s     Error
+</pre>`
+)
 
 func main() {
 	var err error
@@ -43,17 +54,17 @@ func main() {
 		log.Panic("QB_USERNAME env variable not set")
 	}
 
-	qbConnection = qbittorrentapi.NewAPIClient(QB_URL)
-	err = qbConnection.Login(QB_USERNAME, QB_PASSWORD)
+	qb = qbt.NewClient(QB_URL)
+	err = qb.Login(QB_USERNAME, QB_PASSWORD)
 	if err != nil {
 		log.Fatalf("Error during login: %s", err.Error())
 	}
-	torrentList, err := qbConnection.GetTorrentList()
+	torrentList, err := qb.Torrents(qbt.TorrentsOptions{})
 	if err != nil {
 		log.Printf("%v", err.Error())
 	}
-	for _,torrent := range torrentList{
-		log.Printf("%v",torrent.Name)
+	for _, torrent := range torrentList {
+		log.Printf("%v", torrent.Name)
 	}
 
 	bot, err = tgbotapi.NewBotAPI(BOT_TOKEN)
@@ -125,7 +136,10 @@ func handleCommand(chatID int64, command string) error {
 	switch command {
 	case "/start":
 		err = sendStart(chatID)
+	case "/get_torrents":
+		err = getTorrents(chatID)
 	}
+
 	return err
 }
 
@@ -134,5 +148,12 @@ func sendStart(chatID int64) error {
 	msg.ParseMode = tgbotapi.ModeHTML
 	_, err := bot.Send(msg)
 
+	return err
+}
+
+func getTorrents(chatID int64) error {
+	msg := tgbotapi.NewMessage(chatID, torrentList)
+	msg.ParseMode = tgbotapi.ModeHTML
+	_, err := bot.Send(msg)
 	return err
 }
